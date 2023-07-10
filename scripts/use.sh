c95d4cbd60bc5b75e8cf8e9eb34b7f69ad4e5f7f
lmarqs <lucas.marques@medprevonline.com>
2023-07-10 00:24:57 -0300

    make script quieter

--- scripts/use.sh
+++ scripts/use.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -xe
-rm -rf $HOME/foundryvtt/bin/*
+mkdir -p $HOME/foundryvtt/bin
+
+rm -rf $HOME/foundryvtt/bin
+
+mkdir -p $HOME/foundryvtt/bin
 
 unzip -q $HOME/foundryvtt/releases/FoundryVTT-$1.zip -d $HOME/foundryvtt/bin
