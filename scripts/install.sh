fac1073edcc3eb06c4f2b03ed1a5dfcb9e49143e
lmarqs <lucas.marques@medprevonline.com>
2023-07-10 00:54:19 -0300

    install pm2 using template

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,5 +8,3 @@ aws s3 sync s3://$GAME_S3_BUCKET/releases $HOME/foundryvtt/releases
 sudo cp $HOME/foundryvtt/scripts/nginx.conf /etc/nginx/sites-available/default
 
 sudo service nginx restart
-
-npm install pm2@latest -g
