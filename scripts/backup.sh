68198f70de9f70a5f30f511275198d288dabc797
lmarqs <lucas.marques@medprevonline.com>
2023-07-10 00:48:07 -0300

    delete temp files

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -11,3 +11,5 @@ VERSION=$(date +'%Y%m%d%H%M%S')
 tar -czvf $HOME/foundryvtt/tmp/$TENANT-$VERSION.tar.gz $HOME/foundryvtt/data/$TENANT
 
 aws s3 cp $HOME/foundryvtt/tmp/$TENANT-$VERSION.tar.gz s3://$GAME_S3_BUCKET/data/$TENANT/$TENANT-$VERSION.tar.gz
+
+rm $HOME/foundryvtt/tmp/$TENANT-$VERSION.tar.gz
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -11,3 +11,5 @@ rm -rf $HOME/foundryvtt/data/$TENANT
 aws s3 cp s3://$GAME_S3_BUCKET/data/$TENANT/$TENANT-$2.tar.gz $HOME/foundryvtt/tmp/$TENANT-$2.tar.gz
 
 tar -xzf $HOME/foundryvtt/tmp/$TENANT-$VERSION.tar.gz $HOME/foundryvtt/data/$TENANT
+
+rm $HOME/foundryvtt/tmp/$TENANT-$VERSION.tar.gz
